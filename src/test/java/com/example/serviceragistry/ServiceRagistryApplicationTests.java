9d39f16a664d14ee07701997acc6b51371d36e20
abc <abc2@gmail.com>
2024-02-20 14:11:15 +0530

    commit uploded

---
+++ src/test/java/com/example/serviceragistry/ServiceRagistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serviceragistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRagistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
